63ea0f90bd745fc07171f0fede77b4918120246c
antonio Montero <antoniomonforma@gmail.com>
2021-09-29 12:46:55 +0200

    project added

---
+++ ud01-ej04/src/main/java/com/salesianostriana/ud01ej04/Ud01Ej04Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.ud01ej04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud01Ej04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud01Ej04Application.class, args);
+	}
+
+}
---
+++ ud01-ej04/src/test/java/com/salesianostriana/ud01ej04/Ud01Ej04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.ud01ej04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ud01Ej04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
